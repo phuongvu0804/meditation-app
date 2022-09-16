fb79ccfee4d42398fc811e80cf234d743f7f00ab
phuongvu0804 <99994868+phuongvu0804@users.noreply.github.com>
2022-09-16 20:34:41 +0300

    Feat: add toast messages

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const activeSong = $("#audio");
 const ringSound = $("#endMeditation");
 const timmingOption = $(".app-tool__wrapper--timming .app-tool__option-list a");
 const timer = $(".app__timer span");
+const toast = $(".toast");
 
 const songList = [
     {
@@ -33,11 +34,13 @@ const songList = [
     {
         name: "A voice of nature",
         path: "./assets/songs/voice-of-nature-20683.mp3"
-    },
+    }
 ];
 
-const timmingOptions = [ 1, 5, 10, 15, 20 ];
-let selectedTiming = 0;
+const timmingOptions = [ 5, 10, 15, 20, 30 ];
+let startingTime;
+let runningTime = 0;
+let remainingTime = 0;
 let intervalId;
 
 //Render list of songs
@@ -121,23 +124,33 @@ timmingBtn.onclick = () => {
 };
 
 const countDown = () => {
-    let totalTime = selectedTiming * 60;
-    console.log(totalTime)
-    if (totalTime >= 0) {
-        let minutes = Math.floor(totalTime / 60);
-        let seconds = (totalTime % 60).toFixed(0); 
+    runningTime = runningTime * 60;
 
+    if (runningTime >= 0) {
+        let minutes = Math.floor(runningTime / 60);
+        let seconds = (runningTime % 60).toFixed(0); 
+        
         if (seconds < 10) {
             seconds = "0" + seconds;
         };
 
         timer.innerText = `${minutes}:${seconds}`;
-        totalTime--;
-        selectedTiming = totalTime / 60;
+
+        runningTime--;
+        remainingTime = runningTime/startingTime;
+        //Fix the proportion to match with animation's speed
+        remainingTime = remainingTime - ( 1/startingTime) * (1-remainingTime);
+
+        runningTime = runningTime / 60;
     } else {
         clearInterval(intervalId);
         activeSong.pause();
         ringSound.play();
+        showToast({
+            title: "Congratulations",
+            message: `You have completed ${startingTime/60} minutes of meditation`,
+            type: "success",
+        });
     };
 
 };
@@ -148,23 +161,36 @@ const handleSetTime = (id) => {
     timmingBtn.onclick();
 
     //Count Down
-    selectedTiming = Number(id);
-    timer.innerText = `${selectedTiming}:00`;
+    runningTime = Number(id);
+    startingTime = Number(id) * 60;
+    timer.innerText = `${runningTime}:00`;
 
 };
 
 //User starts meditation 
 playBtn.onclick = () => {
-    playBtn.classList.add("hide");
-    pauseBtn.classList.add("show");
+    if (activeSong && startingTime) {
+        playBtn.classList.add("hide");
+        pauseBtn.classList.add("show");
+    
+        //Start song
+        activeSong.play();
+    
+        //Start countDown
+        intervalId = setInterval(() => {
+            countDown()
+            styleCircleCountDown();
+        }, 1000);
 
-    //Start song
-    activeSong.play();
+    } else {
+        showToast({
+            title: "Error",
+            message: "To start the meditation, please choose a song and an amount of time.",
+            type: "error",
+        });
 
-    //Start countDown
-    intervalId = setInterval(() => {
-        countDown()
-    }, 1000);
+    };
+
 };
 
 //User pauses meditation 
@@ -175,4 +201,66 @@ pauseBtn.onclick = () => {
     //Pause song
     activeSong.pause();
     clearInterval(intervalId);
+};
+
+//Style circle
+const styleCircleCountDown = () => {
+    const circleDashArray = `${(remainingTime * 283).toFixed(0)} 283`;
+    $(".base-timer__path-remaining").setAttribute("stroke-dasharray", circleDashArray);
+}
+
+//----------------------------------------------------------------
+//Toast messages display
+const showToast = ({
+    title = "",
+    message = "",
+    type = "info",
+    duration = 3000
+}) => {
+    const toastField = $(".app__toast-msg");
+    if (toastField) {
+        const appearTime = duration + 1000;
+        const delay = (duration/1000).toFixed(2);
+
+        const icons = {
+            infor: "<i class='fa-solid fa-circle-info'></i>",
+            success: "<i class='fa-solid fa-circle-check'></i>",
+            warning: "<i class='fa-solid fa-triangle-exclamation'></i>",
+            error: "<i class='fa-sharp fa-solid fa-circle-exclamation'></i>",  
+        };
+
+        const icon = icons[type];
+        const toast = document.createElement("div");
+        toast.classList.add("toast", `toast--${type}`);
+        toast.style.animation = `slideInLeft ease 0.3s, fadeOut linear 1s ${delay}s forwards`
+
+        toast.innerHTML = `
+            <div class="toast__icon">
+                ${icon}
+            </div>
+            <div class="toast__body">
+                <h3 class="toast__title">${title}</h3>
+            <p class="toast__msg">${message}</p>
+            </div>
+            <div class="toast__close">
+                <i class="fas fa-times"></i>
+            </div> 
+        `;
+        
+        toastField.appendChild(toast);
+
+        //Remove toast message
+        const autoRemoveId = setTimeout(() => {
+            toastField.removeChild(toast);
+            clearTimeout(autoRemoveId);
+        }, appearTime);
+
+        toast.onclick = e => {
+            if (e.target.closest(".toast__close")) {
+                toastField.removeChild(toast);
+                clearTimeout(autoRemoveId);
+            };
+
+        };
+    };
 };
